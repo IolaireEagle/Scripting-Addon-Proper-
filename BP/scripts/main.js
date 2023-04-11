0ae18d6cf5c6aef19075bf4e02d128c224d71ca6
BuildTools <unconfigured@null.spigotmc.org>
2023-04-11 14:33:54 +0100

    added some more basic commands and a fix command that removes all of your vanilla enchantments - NEEDS TO BE FIXED

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,4 +1,4 @@
-import { Container, Player, world, MinecraftEffectTypes } from "@minecraft/server";
+import { Container, Player, world, MinecraftEffectTypes, ItemStack } from "@minecraft/server";
 
 const validEnchants = ["explode"];
 
@@ -60,16 +60,39 @@ function breakCommand(sender) {
 }
 
 function explodeCommand(sender, args) {
-    const block = sender.getBlockFromViewDirection()
-    if (args == "") {
-        args = "4"
+    try {
+        const block = sender.getBlockFromViewDirection()
+        if (args == "") {
+            args = "4"
+        }
+        block.dimension.createExplosion(block.location, parseInt(args))
     }
-    block.dimension.createExplosion(block.location, parseInt(args))
+    catch (e) {
+        sender.sendMessage("§cBlock is out of range!")
+    }
 }
 
 function feedCommand(sender) {
     sender.addEffect(MinecraftEffectTypes.saturation, 2, 255, false)
     sender.playSound("random.burp")
+}
+
+function fixCommand(sender) {
+    try {
+        /** @type {Container} */
+        var container = sender.getComponent("inventory").container;
+        var heldItem = container.getItem(sender.selectedSlot);
+        var newItem = new ItemStack(heldItem.type, heldItem.amount)
+        let enchantComponent = heldItem.getComponent("enchantments")
+        let lore = heldItem.getLore()
+        let newEnchants = newItem.getComponent("enchantments")
+        newEnchants = enchantComponent
+        newItem.setLore(lore)
+        container.setItem(sender.selectedSlot, newItem)
+    }
+    catch (e) {
+        sender.sendMessage("§cFailed to repair! Reason: " + String(e))
+    }
 }
 
 world.events.beforeChat.subscribe((event) => {
@@ -90,15 +113,39 @@ world.events.beforeChat.subscribe((event) => {
             } else if (event.message.includes("day")) {
                 player.runCommandAsync("time set day")
                 player.sendMessage("Set the time to day!")
+
+            } else if (event.message.includes("peaceful")) {
+                player.runCommandAsync("difficulty p")
+                player.sendMessage("Set the difficulty to Peaceful!")
             } else if (event.message.includes("easy")) {
                 player.runCommandAsync("difficulty e")
                 player.sendMessage("Set the difficulty to Easy!")
+            } else if (event.message.includes("normal")) {
+                player.runCommandAsync("difficulty n")
+                player.sendMessage("Set the difficulty to Normal!")
+            } else if (event.message.includes("hard")) {
+                player.runCommandAsync("difficulty h")
+                player.sendMessage("Set the difficulty to Hard!")
             } else if (event.message.includes("explode")) {
                 let lvl = event.message.replace(/[a-z, !]/gi, '')
                 noExclamName = noExclamName.replace("explode", "")
                 explodeCommand(player, lvl)
             } else if (event.message.includes("feed")) {
                 feedCommand(player)
+            } else if (event.message.includes("gmc")) {
+                event.sender.runCommandAsync("gamemode c @s")
+                player.sendMessage("Set own gamemode to Creative!")
+            } else if (event.message.includes("gmsp")) {
+                event.sender.runCommandAsync("gamemode spectator @s")
+                player.sendMessage("Set own gamemode to Spectator!")
+            } else if (event.message.includes("gma")) {
+                event.sender.runCommandAsync("gamemode a @s")
+                player.sendMessage("Set own gamemode to Adventure!")
+            } else if (event.message.includes("gms")) {
+                event.sender.runCommandAsync("gamemode s @s")
+                player.sendMessage("Set own gamemode to Survival!")
+            } else if (event.message.concat(" ").includes("fix ")) {
+                fixCommand(event.sender)
             }
             else {
                 player.sendMessage("§cUnknown command!")
